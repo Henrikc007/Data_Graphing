7946c44aa8f3c0bddb69e2adf93ed5684be63538
Henrikc007 <128123648+Henrikc007@users.noreply.github.com>
2024-10-30 12:46:26 +0100

    Add files via upload

---
+++ hentedata.py
@@ -0,0 +1,64 @@
+
+import pandas as pd
+import hemmeligefiler
+
+ipadresse = hemmeligefiler.ipadresse
+brugernavn = hemmeligefiler.brugernavn
+brugerpw = "alle1sql2specialister!"
+mindatabase = "northwind"
+#connecte til lokal server og lave mysql dataarbejde på denne
+
+#jeg skal lave secret fil, hvor mine koder er indlejret
+
+
+import mysql.connector
+from mysql.connector import Error
+
+# Function to create a connection
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            password=user_password,
+            database=db_name
+        )
+        print("Connection successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+#en funktion som henter data fre en kolonne og returnere dem i en array
+def fetchDataframe(whatTable,whatColoumn):    
+    nortwindConn=create_connection(ipadresse,brugernavn,brugerpw,mindatabase)
+    cursor = nortwindConn.cursor()
+    if whatColoumn == "ALL":
+        enQuery = "select * from "
+        enQuery = enQuery + whatTable
+    else:
+        #snyder en select disticnt istedet fror at lave mit eget valg af sql query
+        enQuery = "select distinct " + whatColoumn + " from " + whatTable
+        
+    result_dataframe = pd.read_sql(enQuery,nortwindConn)
+    cursor.close()
+    nortwindConn.close()
+    return result_dataframe
+
+
+def fetchDataframeRestraint(whatTable,whatColoumn, whatrestraint):
+    nortwindConn=create_connection(ipadresse,brugernavn,brugerpw,mindatabase)
+    cursor = nortwindConn.cursor()
+    enQuery = "select * from "
+    enQuery = enQuery + whatTable
+    result_dataframe = pd.read_sql(enQuery,nortwindConn)
+    cursor.close()
+    nortwindConn.close()
+    return result_dataframe
+
+
+
+
+
+
